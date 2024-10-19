58e39cd18dd32e71d664b28df001a856fb5fa0bc
gram12321 <34036134+gram12321@users.noreply.github.com>
2024-10-19 22:47:52 +0000

    Better companyinfo

--- js/database/loadSidebar.js
+++ js/database/loadSidebar.js
@@ -1,5 +1,5 @@
 function initializeSidebar() {
-    displayCompanyInfo();
+        renderCompanyInfo();
 
     // Attach logout event handler
     const logoutLink = document.getElementById('logout-link');
@@ -12,24 +12,7 @@ function initializeSidebar() {
     }
 }
 
-export function updateCompanyInfo() {
-    const companyName = localStorage.getItem('companyName');
-    const money = localStorage.getItem('money');
-    const companyInfoDiv = document.getElementById('companyInfo');
-
-    if (companyInfoDiv) {
-        companyInfoDiv.innerHTML = `
-          <p><strong>Company Info:</strong> ${companyName}</p>
-          <p><strong>Money:</strong> $${money}</p>
-        `;
-    }
-    const dropdownToggle = document.querySelector('#navbarDropdown');
-    if (companyName && dropdownToggle) {
-        dropdownToggle.textContent = companyName;
-    }
-}
-
-function displayCompanyInfo() {
+export function renderCompanyInfo() {
     const companyName = localStorage.getItem('companyName');
     const money = localStorage.getItem('money');
     const companyInfoDiv = document.getElementById('companyInfo');
@@ -41,7 +24,6 @@ function displayCompanyInfo() {
         `;
     }
 
-    // Update the dropdown menu text
     const dropdownToggle = document.querySelector('#navbarDropdown');
     if (companyName && dropdownToggle) {
         dropdownToggle.textContent = companyName;
--- js/endDay.js
+++ js/endDay.js
@@ -1,10 +1,10 @@
-import {updateCompanyInfo} from './database/loadSidebar.js';
+ import { renderCompanyInfo } from './database/loadSidebar.js';
 export function addMoney() {
     const currentMoney = localStorage.getItem('money');
     if (currentMoney !== null) {
         const newMoney = parseInt(currentMoney, 10) + 10000;
         localStorage.setItem('money', newMoney);
-     updateCompanyInfo(); // Update the company info display
+         renderCompanyInfo(); // Update the company info display
     } 
 }
 
