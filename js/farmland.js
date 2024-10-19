881c39652947d3e52a5409e50da5dc9c58b008a5
gram12321 <34036134+gram12321@users.noreply.github.com>
2024-10-19 21:26:26 +0000

    buyland

---
+++ js/farmland.js
@@ -0,0 +1,12 @@
+// Define the Farmland class
+class Farmland {
+  // Constructor to initialize the properties
+  constructor(country, region, acres) {
+    this.country = country;
+    this.region = region;
+    this.acres = acres;
+  }
+}
+
+// Export the Farmland class to make it available for import in other scripts
+export { Farmland };
