9ebc9f9e086548b181008aeaccc3d3a803eba83a
gram12321 <34036134+gram12321@users.noreply.github.com>
2024-10-20 00:34:32 +0000

    Inventory

---
+++ js/resource.js
@@ -0,0 +1,49 @@
+  class Resource {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+
+  class Inventory {
+    constructor() {
+      this.resources = {};
+    }
+
+    addResource(resource, amount = 0) {
+      if (!this.resources[resource.name]) {
+        this.resources[resource.name] = 0;
+      }
+      this.resources[resource.name] += amount;
+    }
+
+    getResourceAmount(name) {
+      return this.resources[name] || 0;
+    }
+
+    setResourceAmount(name, amount) {
+      this.resources[name] = amount;
+    }
+  }
+
+  function displayInventory(inventory) {
+    const inventoryTableBody = document.getElementById('inventory-table-body');
+    inventoryTableBody.innerHTML = ''; // Clear existing content
+    for (const [name, amount] of Object.entries(inventory.resources)) {
+      const row = document.createElement('tr');
+      row.innerHTML = `<td>${name}</td><td>${amount}</td>`;
+      inventoryTableBody.appendChild(row);
+    }
+  }
+
+  // Instantiate resources
+  const barbera = new Resource('Barbera');
+  const chardonnay = new Resource('Chardonnay');
+
+  // Create player inventory
+  const playerInventory = new Inventory();
+
+  // Add 1 Barbera to the inventory
+  playerInventory.addResource(barbera, 1);
+
+  // Export necessary entities
+  export { Resource, Inventory, displayInventory, playerInventory };
