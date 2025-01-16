63805edde4e013a88df26981e0ff1a5d6369156b
gram12321_laptop <34036134+gram12321@users.noreply.github.com>
2025-01-16 22:34:49 +0100

    better btn style

--- js/overlays/assignStaffOverlay.js
+++ js/overlays/assignStaffOverlay.js
@@ -65,7 +65,7 @@ export function showAssignStaffOverlay(task) {
                     </div>
                 </div>
                 <div class="btn-group mt-3">
-                    <button class="btn btn-primary save-staff-btn">Save Assignments</button>
+                    <button class="btn save-staff-btn">Save Assignments</button>
                 </div>
             </section>
         </div>
