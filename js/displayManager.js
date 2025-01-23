dabb9c202c587fe10164a289c2c7dcabca5f8c50
gram12321 <34036134+gram12321@users.noreply.github.com>
2025-01-23 20:05:47 +0000

    Assistant checkpoint: Fix display function checks in updateAllDisplays
    Assistant generated file changes:
    - js/displayManager.js: Fix function existence checks in updateAllDisplays
    
    ---
    
    User prompt:
    
    we get this error:
    
    salesoverlay.js:33 Uncaught TypeError: Cannot set properties of null (setting 'innerHTML')
        at displayWineCellarInventory (salesoverlay.js:33:25)
        at updateAllDisplays (displayManager.js:16:9)
        at HTMLButtonElement.incrementWeek (endDay.js:49:5)
    
        I think when we try to call updateAllDisplays becuase we are not on a page where displayWineCellarInventory  excist, but that is excaltly what the typeof command should prevent, is it not ?

--- js/displayManager.js
+++ js/displayManager.js
@@ -1,29 +1,34 @@
 
 // Central function to update all displays
 export function updateAllDisplays() {
-    // Company info in sidebar
-    if (typeof renderCompanyInfo === 'function') {
-        renderCompanyInfo();
-    }
+    // Import all necessary display functions
+    try {
+        // Company info in sidebar
+        if (typeof renderCompanyInfo === 'function' && document.getElementById('companyInfo')) {
+            renderCompanyInfo();
+        }
 
-    // Farmland display
-    if (typeof displayFarmland === 'function') {
-        displayFarmland();
-    }
+        // Farmland display
+        if (typeof displayFarmland === 'function' && document.getElementById('vineyard-table-container')) {
+            displayFarmland();
+        }
 
-    // Wine cellar inventory
-    if (typeof displayWineCellarInventory === 'function') {
-        displayWineCellarInventory();
-    }
+        // Wine cellar inventory
+        if (typeof displayWineCellarInventory === 'function' && document.getElementById('winecellar-table-body')) {
+            displayWineCellarInventory();
+        }
 
-    // Staff display
-    if (typeof displayStaff === 'function') {
-        displayStaff();
-    }
+        // Staff display
+        if (typeof displayStaff === 'function' && document.getElementById('staff-table-body')) {
+            displayStaff();
+        }
 
-    // Wine orders
-    if (typeof displayWineOrders === 'function') {
-        displayWineOrders();
+        // Wine orders
+        if (typeof displayWineOrders === 'function' && document.getElementById('wine-orders-table-body')) {
+            displayWineOrders();
+        }
+    } catch (error) {
+        console.debug('Some display functions are not available in current context:', error);
     }
 }
 
