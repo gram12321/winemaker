8f6da94d508edc2485f68840d01cd426212811f0
gram12321_laptop <34036134+gram12321@users.noreply.github.com>
2025-04-09 10:09:53 +0200

    cleaning

--- src/lib/core/utils/utils.ts
+++ src/lib/core/utils/utils.ts
@@ -1,7 +1,5 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
-import { SkillLevels } from "../constants/staffConstants"
-import { getColorClass } from './formatUtils'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
